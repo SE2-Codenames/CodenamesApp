891657c0db108b249ede07ba88c70964a13cef5c
EduardKlaus <klaus.eduard96@gmail.com>
2025-05-07 23:37:31 +0200

    change and optimice MainActivity with a new Navigation class to change and fix problems with the connection. new buttons configuration

--- app/src/main/java/com/example/codenamesapp/MainActivity.kt
+++ app/src/main/java/com/example/codenamesapp/MainActivity.kt
@@ -3,17 +3,15 @@ package com.example.codenamesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.example.codenamesapp.ui.theme.CodenamesAppTheme
 import com.example.codenamesapp.gamelogic.GameManager
-import com.example.codenamesapp.lobby.LobbyScreen
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-
 
 class MainActivity : ComponentActivity() {
 
@@ -27,54 +25,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        gameManager = GameManager{loadWords()}
         gameManager = GameManager { loadWords() }
 
         setContent {
-            val initialGameState = remember {
-                gameManager.startNewGame()
-                gameManager.gameState
-            }
-
-            var gameState by remember { mutableStateOf(initialGameState) }
-            var screen by rememberSaveable { mutableStateOf("menu") }
-
+            val navController = rememberNavController()
             CodenamesAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    when (screen) {
-                        "menu" -> MainMenuScreen(
-                            onPlayClicked = {
-                                screen = "game"
-                            },
-
-                            onRulesClicked = {
-                                screen = "rules"
-                            },
-                            onSettingsClicked = {
-                                screen = "lobby"
-                            }
-                        )
-
-                        "rules" -> RulesScreen(
-                            onBack = { screen = "menu" }
-                        )
-
-                        "game" -> LobbyScreen(
-                            onBackToMain = { screen = "menu" },
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        "lobby" -> Connection(
-                            onBackToMain = { screen = "menu" },
-                            modifier = Modifier.padding(innerPadding)
-                        )
-
-                    }
+                    // Verwende innerPadding hier, um den Hauptinhalt einzurücken
+                    AppNavigation(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
-
     }
 }
-
-
-
