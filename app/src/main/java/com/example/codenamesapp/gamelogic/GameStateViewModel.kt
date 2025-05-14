94a6bcbded2cfe104f2591ae78678491772c51ea
EduardKlaus <klaus.eduard96@gmail.com>
2025-05-14 21:24:23 +0200

    minor change in GameStateView for the PlayerRole and PlayerTeam

--- app/src/main/java/com/example/codenamesapp/gamelogic/GameStateViewModel.kt
+++ app/src/main/java/com/example/codenamesapp/gamelogic/GameStateViewModel.kt
@@ -7,8 +7,12 @@ import com.example.codenamesapp.model.TeamRole
 
 class GameStateViewModel : ViewModel() {
     val payload = mutableStateOf<PayloadResponseMove?>(null)
-    val team = mutableStateOf<TeamRole?>(null)
-    val playerRole = mutableStateOf<Boolean?>(null)
+    val team = mutableStateOf<TeamRole?>(null) // team whose turn it is
+    val playerRole = mutableStateOf(false) // isSpymaster from server (ignore for UI)
+
+    // OWN selections
+    val myTeam = mutableStateOf<TeamRole?>(null)
+    val myIsSpymaster = mutableStateOf(false)
 
     var onShowGameBoard: (() -> Unit)? = null
 }
