426759bc7d1531539240e2d22113caff8e973bf3
stefaniepirhofer <stefanie.pirhofer@outlook.de>
2025-05-25 18:23:27 +0200

    refactoring PlayScreen to MVVM, changed GameStateViewModel and GameManager (does not work yet)

--- app/src/main/java/com/example/codenamesapp/gamelogic/GameStateViewModel.kt
+++ app/src/main/java/com/example/codenamesapp/gamelogic/GameStateViewModel.kt
@@ -4,8 +4,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.codenamesapp.model.PayloadResponseMove
 import com.example.codenamesapp.model.TeamRole
+import com.example.codenamesapp.network.Communication
 
-class GameStateViewModel : ViewModel() {
+class GameStateViewModel(private val gameManager : GameManager) : ViewModel() {
     val payload = mutableStateOf<PayloadResponseMove?>(null)
     val team = mutableStateOf<TeamRole?>(null) // team whose turn it is
     val playerRole = mutableStateOf(false) // isSpymaster from server (ignore for UI)
@@ -15,4 +16,16 @@ class GameStateViewModel : ViewModel() {
     val myIsSpymaster = mutableStateOf(false)
 
     var onShowGameBoard: (() -> Unit)? = null
+
+    fun loadGame() {
+        //gameState.value = gameManager.initGame()
+    }
+
+    fun handleCardClock (index: Int, communication: Communication) {
+        communication.giveCard(index)
+    }
+
+    fun sendHint (hintWord: String, hintNumber: Int, communication: Communication) {
+        communication.giveHint(hintWord, hintNumber)
+    }
 }
