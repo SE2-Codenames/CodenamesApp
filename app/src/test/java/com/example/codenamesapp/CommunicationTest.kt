e49df55dbf844e2d383fb5b1d4cd7db896753a0c
Pablo Pirker Garcia <pablopi@edu.aau.at>
2025-06-08 10:34:24 +0200

    test message data class to complete CommunicationTest

--- app/src/test/java/com/example/codenamesapp/CommunicationTest.kt
+++ app/src/test/java/com/example/codenamesapp/CommunicationTest.kt
@@ -1,6 +1,7 @@
 package com.example.codenamesapp
 
 import com.example.codenamesapp.network.Communication
+import com.example.codenamesapp.network.Message
 import io.mockk.mockk
 import io.mockk.verify
 import okhttp3.WebSocket
@@ -63,5 +64,33 @@ class CommunicationTest {
     fun testSendRaw() {
         communication.send("RAW_MESSAGE")
         verify { mock.send("RAW_MESSAGE") }
+    }
+
+    @Test
+    fun testMessageDataClass() {
+        val message = Message(
+            type = "TEST_TYPE",
+            data = mapOf("key1" to "value1", "key2" to "value2")
+        )
+
+        // Test properties
+        assert(message.type == "TEST_TYPE")
+        assert(message.data == mapOf("key1" to "value1", "key2" to "value2"))
+
+        // Test toString()
+        assert(message.toString().contains("TEST_TYPE"))
+        assert(message.toString().contains("key1"))
+
+        // Test copy()
+        val copied = message.copy(type = "NEW_TYPE")
+        assert(copied.type == "NEW_TYPE")
+        assert(copied.data == message.data)
+
+        // Test equals()
+        val same = Message("TEST_TYPE", mapOf("key1" to "value1", "key2" to "value2"))
+        assert(message == same)
+
+        val different = Message("OTHER_TYPE", emptyMap())
+        assert(message != different)
     }
 }
